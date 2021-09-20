308907c398a936846639a98e03e2acf0ffe7b02c
infjbunny <74402709+infjbunny@users.noreply.github.com>
2021-09-20 19:03:27 -0400

    Add files via upload

---
+++ configuration.js
@@ -0,0 +1,659 @@
+settings = {
+    "title": "Bunny's Socionics Test",
+    "description": "This test will score the likelihood of each Socionics dichotomy based on your answers. The highest results will be the most likely. The lowest results will be the least likely.",
+    "numOptions": 2
+}
+
+
+questions = [
+{
+    "description":"It is better if I think in advance. OR It is better if I act on a situation.",
+    "weights":{
+    "Rational":1
+},
+	"aw": {
+	"Irrational": 1
+}
+},
+{
+    "description":"I’m consistent and reliable but not flexible. OR I’m flexible and changeable but lack consistency.",
+    "weights":{
+    "Rational":1
+},
+	"aw": {
+	"Irrational": 1
+}
+},
+{
+    "description":"I calm down only when everything is established. OR I cannot live without a constant change of impressions.",
+    "weights":{
+    "Rational":1
+},
+	"aw": {
+	"Irrational": 1
+}
+},
+{
+    "description":"I have a stable capacity for work. OR I live in rhythmic ups and downs.",
+    "weights":{
+    "Rational":1
+},
+	"aw": {
+	"Irrational": 1
+}
+},
+{
+    "description":"My mood if nobody spoils it will be stable. OR My mood is unstable.",
+    "weights":{
+    "Rational":1
+},
+	"aw": {
+	"Irrational": 1
+}
+},
+{
+    "description":"Norms and obligations organize my work. OR Freedom from obligations is vitally important for me.",
+    "weights":{
+    "Rational":1
+},
+	"aw": {
+	"Irrational": 1
+}
+},
+{
+    "description":"I can hardly change my decision if it is justified. OR I easily refuse a decision if conditions have changed.",
+    "weights":{
+    "Rational":1
+},
+	"aw": {
+	"Irrational": 1
+}
+},
+{
+    "description":"I’m constantly tense somewhat more than necessary. OR I’m constantly relaxed somewhat more than necessary.",
+    "weights":{
+    "Rational":1
+},
+	"aw": {
+	"Irrational": 1
+}
+},
+{
+    "description":"I explain better than notice. OR I notice better than explain.",
+    "weights":{
+    "Rational":1
+},
+	"aw": {
+	"Irrational": 1
+}
+},
+{
+    "description":"I do not tolerate sharp unforeseen turns. OR Unexpected change of my plans is usual for me.",
+    "weights":{
+    "Rational":1
+},
+	"aw": {
+	"Irrational": 1
+}
+},
+{
+    "description":"I easily adhere to a solid schedule. OR I cannot do one thing for a long time even if I need to.",
+    "weights":{
+    "Rational":1
+},
+	"aw": {
+	"Irrational": 1
+}
+},
+{
+    "description":"My efforts are distributed evenly. OR First I start and then I make up for lost time.",
+    "weights":{
+    "Rational":1
+},
+	"aw": {
+	"Irrational": 1
+}
+},
+{
+    "description":"Others consider me consistent. OR Others consider me flexible.",
+    "weights":{
+    "Rational":1
+},
+	"aw": {
+	"Irrational": 1
+}
+},
+{
+    "description":"I do not start something new until I’ve finished the previous task. OR Usually I start several projects at once.",
+    "weights":{
+    "Rational":1
+},
+	"aw": {
+	"Irrational": 1
+}
+},
+{
+    "description":"A bad mood has little impact on my productivity. OR I’m strongly dependent on my internal biorhythms.",
+    "weights":{
+    "Rational":1
+},
+	"aw": {
+	"Irrational": 1
+}
+},
+{
+    "description":"I am a person of duty and discipline. OR Changes in my mood are not clear to those around me.",
+    "weights":{
+    "Rational":1
+},
+	"aw": {
+	"Irrational": 1
+}
+},
+{
+    "description":"I judge better than I perceive. OR I perceive better than judge.",
+    "weights":{
+    "Rational":1
+},
+	"aw": {
+	"Irrational": 1
+}
+},
+{
+    "description":"I do not feel well when there is confusion around me. OR I quietly maneuver between opposites.",
+    "weights":{
+    "Rational":1
+},
+	"aw": {
+	"Irrational": 1
+}
+},
+{
+    "description":"I make contacts easier than break them up. OR I expect others to initiate or establish contacts.",
+    "weights":{
+    "Extroversion": 1
+},
+	"aw":{
+	"Introversion": 1
+}
+},
+{
+    "description":"I have a very broad soul. OR I cannot bring myself to reveal myself.",
+    "weights":{
+    "Extroversion": 1
+},
+    "aw":{
+	"Introversion":1
+}
+},
+{
+    "description":"The scope of my activities is growing freely. OR My activities are directed in depth not in breadth.",
+    "weights":{
+    "Extroversion": 1
+},
+    "aw":{
+	"Introversion":1
+}
+},
+{
+    "description":"I start up more and more projects. OR I limit the scope of duties when possible.",
+    "weights":{
+    "Extroversion": 1
+},
+    "aw":{
+	"Introversion":1
+}
+},
+{
+    "description":"I spend energy without hesitation. OR It’s easy for me to save energy.",
+    "weights":{
+    "Extroversion": 1
+},
+    "aw":{
+	"Introversion":1
+}
+},
+{
+    "description":"I am pleased to tell or show. OR I feel more confident when I listen or watch.",
+    "weights":{
+    "Extroversion": 1
+},
+    "aw":{
+	"Introversion":1
+}
+},
+{
+    "description":"I am an open person. OR I am a reserved person.",
+    "weights":{
+    "Extroversion": 1
+},
+    "aw":{
+	"Introversion":1
+}
+},
+{
+    "description":"My activities expand in breadth. OR I do not expand until I master what I’ve acquired.",
+    "weights":{
+    "Extroversion": 1
+},
+    "aw":{
+	"Introversion":1
+}
+},
+{
+    "description":"I work confidently when I initiate contacts. OR I work confidently when there are fewer contacts.",
+    "weights":{
+    "Extroversion": 1
+},
+    "aw":{
+	"Introversion":1
+}
+},
+{
+    "description":"I use information more than I save. OR I save information more than I use.",
+    "weights":{
+    "Extroversion": 1
+},
+    "aw":{
+	"Introversion":1
+}
+},
+{
+    "description":"I solve external problems faster than internal ones. OR I solve internal problems faster than external ones.",
+    "weights":{
+    "Extroversion": 1
+},
+    "aw":{
+	"Introversion":1
+}
+},
+{
+    "description":"I completely forget myself in my workflow. OR I can stop quickly even when highly interested.",
+    "weights":{
+    "Extroversion": 1
+},
+    "aw":{
+	"Introversion":1
+}
+},
+{
+    "description":"I am more around myself than in myself. OR I am more in myself than around myself.",
+    "weights":{
+    "Extroversion": 1
+},
+    "aw":{
+	"Introversion":1
+}
+},
+{
+    "description":"I give more energy than I save. OR I save more energy than I give.",
+    "weights":{
+    "Extroversion": 1
+},
+    "aw":{
+	"Introversion":1
+}
+},
+{
+    "description": " It is easier for me to understand others than myself. OR I understand my motives more easily than the actions of others. ",
+"weights":{
+"Extroversion": 1
+},
+    "aw":{
+	    "Introversion":1
+}
+},
+{
+    "description":"I spend more energy than I accumulate. OR I save more energy than I waste.",
+    "weights":{
+    "Extroversion": 1
+},
+    "aw":{
+	"Introversion":1
+}
+},
+{
+    "description":"Contacts with strangers do not bother me. OR Contacts with strangers require effort.",
+    "weights":{
+    "Extroversion": 1
+},
+    "aw":{
+	"Introversion":1
+}
+},
+{
+    "description":"Often I regret excessive activity. OR Often I regret underdeveloped activity.",
+    "weights":{
+    "Extroversion": 1
+},
+    "aw":{
+	"Introversion":1
+}
+},
+{
+    "description":"If necessary I take (physical) care of myself easily. OR I take care of myself with great reluctance.",
+    "weights":{
+    "Sensing": 1
+},
+	"aw":{
+	"Intuition": 1
+}
+},
+{
+    "description":"I use what I have skillfully. OR I imagine more than I have in reality.",
+    "weights":{
+    "Sensing": 1
+},
+	"aw":{
+	"Intuition": 1
+}
+},
+{
+    "description":"With my practicality I will not die anywhere. OR My impracticality bothers my loved ones.",
+    "weights":{
+    "Sensing": 1
+},
+	"aw":{
+	"Intuition": 1
+}
+},
+{
+    "description":"I am more successful in doing what is proven and reliable. OR I’m more effective in unusual incomprehensible situations.",
+    "weights":{
+    "Sensing": 1
+},
+	"aw":{
+	"Intuition": 1
+}
+},
+{
+    "description":"I get the tangible result today. OR I postpone daily requests for tomorrow.",
+    "weights":{
+    "Sensing": 1
+},
+	"aw":{
+	"Intuition": 1
+}
+},
+{
+    "description":"I trust acquired experience not intuition. OR I rely on my assumptions and guesses.",
+    "weights":{
+    "Sensing": 1
+},
+	"aw":{
+	"Intuition": 1
+}
+},
+{
+    "description":"My practical skills are stronger than my imagination. OR My imagination is stronger than my practical skills.",
+    "weights":{
+    "Sensing": 1
+},
+	"aw":{
+	"Intuition": 1
+}
+},
+{
+    "description":"I feel my body; I stand firmly on my feet. OR Plunging into my world I can forget about the most urgent physical needs.",
+    "weights":{
+    "Sensing": 1
+},
+	"aw":{
+	"Intuition": 1
+}
+},
+{
+    "description":"I am a precise and realistic person. OR Ideas and original assumptions are my areas.",
+    "weights":{
+    "Sensing": 1
+},
+	"aw":{
+	"Intuition": 1
+}
+},
+{
+    "description":"I proceed from an actual state of affairs. OR I proceed from possibilities and prospects.",
+    "weights":{
+    "Sensing": 1
+},
+	"aw":{
+	"Intuition": 1
+}
+},
+{
+    "description":"I get more joy from physical activity. OR I receive more joy from a sudden idea.",
+    "weights":{
+    "Sensing": 1
+},
+	"aw":{
+	"Intuition": 1
+}
+},
+{
+    "description":"I do more than I create. OR I think more than I do.",
+    "weights":{
+    "Sensing": 1
+},
+	"aw":{
+	"Intuition": 1
+}
+},
+{
+    "description":"I notice everything that happens around me. OR I see only what I attach importance to.",
+    "weights":{
+    "Sensing": 1
+},
+	"aw":{
+	"Intuition": 1
+}
+},
+{
+    "description":"My offers are reliable. OR My proposals are unusual.",
+    "weights":{
+    "Sensing": 1
+},
+	"aw":{
+	"Intuition": 1
+}
+},
+{
+    "description":"I see better ways of achieving a goal. OR I see a goal better than the ways to achieve it.",
+    "weights":{
+    "Sensing": 1
+},
+	"aw":{
+	"Intuition": 1
+}
+},
+{
+    "description":"If nothing is clear I actively collect information. OR If nothing is clear I rely on my intuition.",
+    "weights":{
+    "Sensing": 1
+},
+	"aw":{
+	"Intuition": 1
+}
+},
+{
+    "description":"When I discuss I state everything unchanged. OR When I discuss I think up interesting details.",
+    "weights":{
+    "Sensing": 1
+},
+	"aw":{
+	"Intuition": 1
+}
+},
+{
+    "description":"I am more of an operator than an experimenter. OR I am more of an experimenter than a user.",
+    "weights":{
+    "Sensing": 1
+},
+	"aw":{
+	"Intuition": 1
+}
+},
+{
+    "description":"My behavior is unemotional. OR I experience feelings deeply.",
+    "weights":{
+    "Logical": 1
+},
+	"aw":{
+	"Ethical": 1
+}
+},
+{
+    "description":"My prudence is appreciated more than feelings. OR My humanity is valued more than reasoning.",
+    "weights":{
+    "Logical": 1
+},
+	"aw":{
+	"Ethical":1
+}
+},
+{
+    "description":"I rely on criteria and calculations. OR I feel good attitudes and sympathies.",
+    "weights":{
+    "Logical": 1
+},
+	"aw":{
+	"Ethical":1
+}
+},
+{
+    "description":"I thank people with restraint. OR I respond emotionally.",
+    "weights":{
+    "Logical": 1
+},
+	"aw":{
+	"Ethical":1
+}
+},
+{
+    "description":"I am moved by necessity and then sympathy. OR I am moved by sympathy and then necessity.",
+    "weights":{
+    "Logical": 1
+},
+	"aw":{
+	"Ethical":1
+}
+},
+{
+    "description":"I live more by reason than by heart. OR I live more with my heart than with my mind.",
+    "weights":{
+    "Logical": 1
+},
+	"aw":{
+	"Ethical":1
+}
+},
+{
+    "description":"I pay more attention to validity and usefulness. OR I pay more attention to good and evil.",
+    "weights":{
+    "Logical": 1
+},
+	"aw":{
+	"Ethical":1
+}
+},
+{
+    "description":"The coldness of people does not bother me. OR I worry if nearby people show coldness.",
+    "weights":{
+    "Logical": 1
+},
+	"aw":{
+	"Ethical":1
+}
+},
+{
+    "description":"I have a low amount of emotions in my voice. OR There are many shades in my voice.",
+    "weights":{
+    "Logical": 1
+},
+	"aw":{
+	"Ethical":1
+}
+},
+{
+    "description":"I react more to the content of the conversation. OR I’m very responsive to the intonation of the speaker.",
+    "weights":{
+    "Logical": 1
+},
+	"aw":{
+	"Ethical":1
+}
+},
+{
+    "description":"I’d rather express my thoughts than convey my mood. OR I’d rather convey my mood than lay out my thoughts.",
+    "weights":{
+    "Logical": 1
+},
+	"aw":{
+	"Ethical":1
+}
+},
+{
+    "description":"It is better to prove than to persuade. OR It is better to persuade than to prove.",
+    "weights":{
+    "Logical": 1
+},
+	"aw":{
+	"Ethical":1
+}
+},
+{
+    "description":"I react sharply to absurdity and irrationality. OR I am sensitive to rudeness and ingratitude.",
+    "weights":{
+    "Logical": 1
+},
+	"aw":{
+	"Ethical":1
+}
+},
+{
+    "description":"I choose friends more by their reliability than by my sympathy. OR I choose friends more by my sympathy than their reliability.",
+    "weights":{
+    "Logical": 1
+},
+	"aw":{
+	"Ethical":1
+}
+},
+{
+    "description":"Emotions do not affect me deeply. OR Feelings and impressions accompany my actions.",
+    "weights":{
+    "Logical": 1
+},
+	"aw":{
+	"Ethical":1
+}
+},
+{
+    "description":"I am pleased to analyze and order. OR I am pleased to communicate with good people.",
+    "weights":{
+    "Logical": 1
+},
+	"aw":{
+	"Ethical":1
+}
+},
+{
+    "description":"I begin with an analysis of a situation. OR I begin by establishing a good relationship.",
+    "weights":{
+    "Logical": 1
+},
+	"aw":{
+	"Ethical":1
+}
+},
+{
+    "description":"I gain more respect using competence and intelligence. OR I attract people more by humanity and sympathy.",
+    "weights":{
+    "Logical": 1
+},
+	"aw":{
+	"Ethical":1
+}
+}
+
+]
+
